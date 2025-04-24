6daff39ba503bbd5b41f047db46a0db2ff033fc2
Joshua Goon <jgoon@tesla.com>
2025-04-23 18:09:31 -0700

    naive bot

---
+++ bot.py
@@ -0,0 +1,26 @@
+# game_state
+# state.logs = (yourmove, outcome = "win"| "loss" | "tie")[]
+# state.round = number
+
+# make_move(game_state) -> 0, 1, 2 = rock, paper, scissors
+
+# Example-
+
+
+class rps_bot:
+    self.s = [0, 0, 0]
+    self.o = { "win": 1, "loss": -1, "tie": 0 }
+
+    def best_outcome(self):
+        m = max(self.s)
+
+        for i, v in enumerate(self.s):
+            if v == m: return i
+
+        return 0
+
+    def make_move(self, game_state=None):
+        move, outcome = game_state.logs[-1]
+        self.s[move] += self.o[outcome]
+        return self.best_outcome()
+
